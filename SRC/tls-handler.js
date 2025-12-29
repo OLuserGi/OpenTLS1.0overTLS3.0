afb32130ecc54fead8821ba8750e08757282ccd6
OLuserGi <sergio_lu@mail.ru>
2025-12-29 16:18:13 +0500

    Create tls-handler.js

---
+++ SRC/tls-handler.js
@@ -0,0 +1,9 @@
+export class TLSHandler {
+    static negotiateConnection(url) {
+        // Механизм согласования соединения
+    }
+
+    static fallbackToLegacyProtocol(url) {
+        // Резервный механизм подключения
+    }
+}
